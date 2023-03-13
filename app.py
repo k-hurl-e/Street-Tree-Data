8673264504dd4f9f6f2f9e2a2df8bb51be8bc274
keenan <62031695+keenan-h@users.noreply.github.com>
2023-03-13 17:47:53 -0400

    Add existing project files to Git

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, url_for, redirect, Markup
+from nta_lists import boro_lists
+from treedata import nta_table_maker
+
+app = Flask(__name__)
+
+def clean(input):
+    return input.strip(",/()")
+
+@app.route('/', methods=["GET", "POST"])
+def pick_nta():
+    if request.method == "POST":
+       # getting input with name = lname in HTML form
+       nta_select = clean(request.form.get("Neighborhood"))
+       table = nta_table_maker(nta_select, 10)
+       return redirect(url_for('results', table=table, nta_select=nta_select))
+    return render_template("index.html", boro_lists=boro_lists)
+
+@app.route('/results')
+def results():
+    table = Markup(request.args.get('table', None))
+    nta_select = request.args.get('nta_select', None)
+    return render_template('results.html', table=table, nta_select=nta_select) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ treedata.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sodapy import Socrata
+from nta_lists import boro_lists
+from config import TOKEN
+
+client = Socrata("data.cityofnewyork.us", TOKEN)
+results = client.get("uvpi-gqnh", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+droplist = []
+keepers = ['tree_id', 'spc_common', 'spc_latin', 'health', 'status','boroname', 'nta_name']
+
+for label, content in results_df.items():
+   if label not in keepers:
+     droplist.append(label)
+
+df = results_df.drop(droplist, 1)
+
+def nta_table_maker(nta, row_length):
+    neighborhood_df = df[(df.nta_name == nta)]
+    treecount = {}
+    captrees = []
+    for index, row in neighborhood_df.iterrows():
+        if row['spc_common'] not in treecount:
+            treecount[row['spc_common']] = [1]
+            captrees.append(str(row['spc_common']).title())
+        else: 
+            treecount[row['spc_common']][0] += 1
+
+    cap_dict = dict(zip(captrees, list(treecount.values())))
+    treecount_sorted = dict(sorted(cap_dict.items(), key=lambda x: x[1], reverse=True))
+
+    trees_df_raw = pd.DataFrame.from_dict(treecount_sorted)
+    trees_df = trees_df_raw.T
+    trees_df.rename(columns={0: 'Number of Trees'}, inplace=True)
+    trees_df.drop(['Nan'], 0, inplace=True)
+    trees = trees_df.head(row_length)
+    trees_html = trees.to_html()
+    return(trees_html)
+
+
+# print(neighborhood_df.tree_id.nunique())
+
